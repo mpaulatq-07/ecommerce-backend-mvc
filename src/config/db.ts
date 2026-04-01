70162904f06ff01c6525c2ab8c608ab11edcb440
mpaulatq-07 <mpaula.tq@gmail.com>
2026-04-01 11:13:47 -0500

    feat: implementación inicial MVC con MySQL y Sequelize

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,18 @@
-import mysql from "mysql2/promise";
-
 // Creamos el pool de conexiones
+
 const connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "12345",
-  database: "ecommerce",
+
+host: "localhost",
+
+user: "root",
+
+password: "12345",
+
+database: "ecommerce",
+
 });
 
-export default connection;
 
+
+export default connection;
 
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
-
-
